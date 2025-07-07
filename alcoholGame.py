f3de9ff1f78694729f05c59ac3027c91500abc13
gyubuk <g1370200@gmail.com>
2025-07-08 00:49:53 +0900

    [fix]시장에 가면 수정

--- alcoholGame.py
+++ alcoholGame.py
@@ -170,8 +170,7 @@ def play_game(player, game_people_list):
                     strawberry_game(current_player,game_people_list)
                     break
                 case "4":
-                    market_game(game_people_list)
-                    break
+                    market_game(player, game_people_list)
                 case "5":
                     korean_game(player, game_people_list) #tofu_game에서 korean_game으로 수정. 함수 파라미터 추가
                 case _:
